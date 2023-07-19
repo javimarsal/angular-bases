0399f9c8e672fbbb068e34f02505b69daf51aa5f
javimarsal <jmaus@duck.com>
2023-07-19 13:39:29 +0200

    Add Tarea sobre modulos

--- src/app/counter/counter.module.ts
+++ src/app/counter/counter.module.ts
@@ -1,5 +1,6 @@
-import { CounterComponent } from './components/counter/counter.component';
 import { NgModule } from "@angular/core";
+
+import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
   declarations: [

03b6004a7f4e13ce28257a9b0f6461fddcdb2d5f
javimarsal <jmaus@duck.com>
2023-07-19 11:09:21 +0200

    Add Tarea - Cambiar nombre y edad

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -16,4 +16,12 @@ export class HeroComponent {
   getHeroDescription (): string {
     return `${this.name} - ${this.age}`
   }
+
+  changeHero (): void {
+    this.name = 'spiderman'
+  }
+
+  changeAge (): void {
+    this.age = 20
+  }
 }

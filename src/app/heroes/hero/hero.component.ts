6cdb900f551fa4b773025567a41928be8c061d86
javimarsal <jmaus@duck.com>
2023-07-19 10:21:49 +0200

    Add Interpolacion, estructura HTML y estilos

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
+  public name:  string = 'ironman'
+  public age:   number = 45
 
 }

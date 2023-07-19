9096ba2dfce04af25aacbba3c5e68abd76f866c2
javimarsal <jmaus@duck.com>
2023-07-19 10:42:59 +0200

    Add One way data binding - enlazado en una sola via

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -9,4 +9,11 @@ export class HeroComponent {
   public name:  string = 'ironman'
   public age:   number = 45
 
+  get capitalizedName (): string {
+    return this.name.toUpperCase()
+  }
+
+  getHeroDescription (): string {
+    return `${this.name} - ${this.age}`
+  }
 }
